07c332fccdd4d4611175be37323c945248d79255
Oliver Aco <benavidez.jemver@gmail.com>
2022-03-09 12:09:18 +0800

    Correcting the another path of fetchAPI import

--- pages/search.js
+++ pages/search.js
@@ -6,7 +6,7 @@ import { BsFilter } from 'react-icons/bs';
 
 import Property from '../components/Property';
 import SearchFilters from '../components/SearchFilters';
-import { baseUrl, fetchApi } from '../utils/fetchApi';
+import { baseUrl, fetchApi } from '../utils/fetchAPI';
 import noresult from '../assets/images/noresult.svg'
 
 const Search = ({ properties }) => {
